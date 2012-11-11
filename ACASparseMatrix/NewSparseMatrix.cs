54b41df0d65ee683dd0db5ae5f2b74a77329d947
Yarko Ryabyy <jarkorjabyy@gmail.com>
2012-11-11 14:25:45 +0200

    inheritance from matrix added

--- ACASparseMatrix/NewSparseMatrix.cs
+++ ACASparseMatrix/NewSparseMatrix.cs
@@ -10,11 +10,35 @@ namespace ACASparseMatrix
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
-
+    using MathNet.Numerics.LinearAlgebra.Double;
     /// <summary>
     /// TODO: Update summary.
     /// </summary>
-    public class NewSparseMatrix
+    public class NewSparseMatrix:Matrix
     {
+        public NewSparseMatrix():base(1,1)
+        {
+
+        }
+
+        public override double At(int row, int column)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void At(int row, int column, double value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MathNet.Numerics.LinearAlgebra.Generic.Vector<double> CreateVector(int size)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MathNet.Numerics.LinearAlgebra.Generic.Matrix<double> CreateMatrix(int numberOfRows, int numberOfColumns)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
