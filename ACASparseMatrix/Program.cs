6d384ebf7d52fa005297c5a5a4f2c8f9bb865e0e
Yarko Ryabyy <jarkorjabyy@gmail.com>
2012-11-11 12:41:58 +0200

    Add new files

---
+++ ACASparseMatrix/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace ACASparseMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DenseVector d = new DenseVector(20);
+
+            foreach (double t in d)
+            {
+                Console.WriteLine(t);
+            }
+        }
+    }
+}
